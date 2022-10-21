5793a531d41d52c65c1df22ccd1c53043b93b3d4
Javivy <84439052+Javivy@users.noreply.github.com>
2022-10-21 19:47:37 -0400

    Delete files for the pull request

--- src/modules/countComment.js
+++
@@ -1,8 +0,0 @@
-const cmtCount = () => {
-  const comments = document.querySelectorAll('.comment-container');
-  const totalComments = document.querySelector('.total-comments');
-  totalComments.textContent = `(${comments.length})`;
-  return comments.length;
-};
-
-export default cmtCount;
