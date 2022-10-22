8d837f5b475210522d762b63547f5f0d5d0d1027
Javivy <84439052+Javivy@users.noreply.github.com>
2022-10-22 02:28:39 -0400

    Build webpack project in production mode

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     filename: '[name].js',
