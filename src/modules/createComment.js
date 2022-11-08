7adfaedd2a48bd235932c4992e93f46c5af664f9
Javivy <84439052+Javivy@users.noreply.github.com>
2022-11-07 21:48:05 -0400

    Add comment validation to don't accept empty comments

--- src/modules/createComment.js
+++ src/modules/createComment.js
@@ -7,26 +7,34 @@ const createComment = async (id) => {
     const nameInput = document.querySelector('.username');
     const commentInput = document.querySelector('.text');
     document.querySelector(`.submit-${id}`).addEventListener('click', async () => {
-      const comment = await addComment(nameInput.value, commentInput.value, id);
-      if (comment.status === 201) {
-        const successMsg = document.querySelector('.success-msg');
-        successMsg.style.display = 'block';
+      if (nameInput.value !== '' && commentInput.value !== '') {
+        const comment = await addComment(nameInput.value, commentInput.value, id);
+        if (comment.status === 201) {
+          const successMsg = document.querySelector('.success-msg');
+          successMsg.style.display = 'block';
+          setTimeout(() => {
+            successMsg.style.display = 'none';
+          }, 4000);
+        }
+        document.querySelector('.all-comments').innerHTML = '';
+        await getComments(id).then((res) => res.forEach((comment) => {
+          document.querySelector('.all-comments').innerHTML += `
+        <div class="comment-container">
+          <p class="comment-name">Name: ${comment.username}</p>
+          <p class="comment-content">${comment.comment}</p>
+          <p class="comment">Date: ${comment.creation_date}</p>
+        </div>`;
+        }));
+        cmtCount();
+        nameInput.value = '';
+        commentInput.value = '';
+      } else {
+        const errorMsg = document.querySelector('.error-msg');
+        errorMsg.style.display = 'block';
         setTimeout(() => {
-          successMsg.style.display = 'none';
-        }, 3000);
+          errorMsg.style.display = 'none';
+        }, 4000);
       }
-      document.querySelector('.all-comments').innerHTML = '';
-      await getComments(id).then((res) => res.forEach((comment) => {
-        document.querySelector('.all-comments').innerHTML += `
-      <div class="comment-container">
-        <p class="comment-name">Name: ${comment.username}</p>
-        <p class="comment-content">${comment.comment}</p>
-        <p class="comment">Date: ${comment.creation_date}</p>
-      </div>`;
-      }));
-      cmtCount();
-      nameInput.value = '';
-      commentInput.value = '';
     });
   }
 };
